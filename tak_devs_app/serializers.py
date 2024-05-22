21f8adde5d59562a16db89bcf1e6893ec212ef0e
Martin Rhine <tusingwiremartinrhinetreviz@gmail.com>
2024-05-23 00:35:05 +0300

    changed image ids to s3 buckets

--- tak_devs_app/serializers.py
+++ tak_devs_app/serializers.py
@@ -3,19 +3,19 @@ from .models import ContactInfo, Project, TeamMember, TechStack, Testimonial, Ga
 
 
 class TeamMemberSerializer(serializers.ModelSerializer):
-    profile_picture =  serializers.CharField(source='profile_picture.public_id', read_only=True)
+    # profile_picture =  serializers.CharField(source='profile_picture.public_id', read_only=True)
     class Meta:
         model = TeamMember
         fields = '__all__'
 
 class TestimonialSerializer(serializers.ModelSerializer):
-    user_photo = serializers.CharField(source='user_photo.public_id', read_only=True)
+    # user_photo = serializers.CharField(source='user_photo.public_id', read_only=True)
     class Meta:
         model = Testimonial
         fields = '__all__'
 
 class GallerySerializer(serializers.ModelSerializer):
-    image = serializers.CharField(source='image.public_id', read_only=True)
+    # image = serializers.CharField(source='image.public_id', read_only=True)
     class Meta:
         model = Gallery
         fields = '__all__'
@@ -41,19 +41,19 @@ class WorkExperienceSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class MobileApplicationSerializer(serializers.ModelSerializer):
-    icon = serializers.CharField(source='icon.public_id', read_only=True)
+    # icon = serializers.CharField(source='icon.public_id', read_only=True)
     class Meta:
         model = MobileApplication
         fields = '__all__'
 
 class DesktopApplicationSerializer(serializers.ModelSerializer):
-    icon = serializers.CharField(source='icon.public_id', read_only=True)
+    # icon = serializers.CharField(source='icon.public_id', read_only=True)
     class Meta:
         model = DesktopApplication
         fields = '__all__'
 
 class WebApplicationSerializer(serializers.ModelSerializer):
-    icon = serializers.CharField(source='icon.public_id', read_only=True)
+    # icon = serializers.CharField(source='icon.public_id', read_only=True)
     class Meta:
         model = WebApplication
         fields = '__all__'
@@ -68,7 +68,7 @@ class ProjectSerializer(serializers.ModelSerializer):
     desktop_applications = DesktopApplicationSerializer(many=True, read_only=True)
     web_applications = WebApplicationSerializer(many=True, read_only=True)
     tech_stack = TechStackSerializer(many=True)
-    project_background_image =  serializers.CharField(source='project_background_image.public_id', read_only=True)
+    # project_background_image =  serializers.CharField(source='project_background_image.public_id', read_only=True)
 
 
     class Meta:
