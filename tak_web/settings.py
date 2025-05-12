741c4a3e96c0d2aee44f49da26f555ddf4280ec2
Palnetguy <tusingwiremartinrhinetreviz@gmail.com>
2025-05-13 00:36:27 +0300

    update settings

--- tak_web/settings.py
+++ tak_web/settings.py
@@ -30,10 +30,10 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = False
 
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['takkinship-backend.up.railway.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
