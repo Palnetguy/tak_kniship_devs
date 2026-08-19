fd0ca7508e7a1f47ea54d5935c3006b03fd8bb00
Palnetguy <tusingwiremartinrhinetreviz@gmail.com>
2026-08-19 16:35:39 +0200

    fix: make SMTP transport configurable

--- tak_web/settings.py
+++ tak_web/settings.py
@@ -244,8 +244,9 @@ STORAGES = {
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_USE_SSL = True
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False").lower() == "true"
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True").lower() == "true"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_TIMEOUT = 10
